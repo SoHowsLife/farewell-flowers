39fca2b7b17f13b76756d6e4f8150aec05ec8596
Scott Wu <SoHowsLife924@gmail.com>
2026-08-16 23:41:32 -0700

    Simple Horizontal Player Movement

---
+++ Assets/Player/PlayerScript/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private SpriteRenderer _sprite;
+    [SerializeField] private Rigidbody2D _body;
+    [SerializeField] private float _speed = 5.0f;
+
+    private float threshold = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float direction = Input.GetAxis("Horizontal");
+        _body.velocity = new Vector2(direction * _speed, 0f);
+        if(direction > threshold)
+        {
+            _sprite.flipX = false;
+        }
+        else if(direction < threshold)
+        {
+            _sprite.flipX = true;
+        }
+    }
+}
